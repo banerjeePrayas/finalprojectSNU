1d99961f12a26f36006b5d13424426fbb8fce320
Prayas Banerjee <rupanreal7@gmail.com>
2022-06-06 23:21:04 +0530

    os

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def after():
     return render_template('after.html', data=final_prediction)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
